ec35bd888c915a40a7d8bc6357ec88417aac8b31
mansy <shaidullinmansur@gmail.com>
2021-11-22 23:38:20 +0300

    end

--- src/lesson6/task2/MyList.java
+++ src/lesson6/task2/MyList.java
@@ -1,8 +1,5 @@
 package lesson6.task2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class MyList<E> {
     Node<E> list = new Node<E>();
 
@@ -24,6 +21,12 @@ public class MyList<E> {
         point.next = u;
     }
 
+    @SafeVarargs
+    public final void add(E... a) {
+        for (E item : a)
+            add(item);
+    }
+
     public void remove(int index) {
         Node<E> point = list;
         int i = 0;
@@ -32,10 +35,13 @@ public class MyList<E> {
         point.next = point.next.next;
     }
 
-   public void remove(E item) {
+    public void remove(E item) {
         Node<E> point = list;
-        while (point.next.data != item)
+        while (point.next.data != item) {
+            if (point.next.next == null)
+                return;
             point = point.next;
+        }
         point.next = point.next.next;
     }
 
@@ -49,38 +55,18 @@ public class MyList<E> {
 
     public void clear() {
         list.data = null;
-        while (list.next != null){
+        while (list.next != null) {
             list = list.next;
             list.data = null;
         }
     }
 
     public void print() {
-        int i = 0;
         Node<E> point = list;
         while (point.next != null) {
-            ++i;
             point = point.next;
             System.out.print(point.data + " ");
         }
-        System.out.println("\ni " + i);
-    }
-
-    public static void main(String[] msi) {
-        MyList<Integer> test = new MyList<>();
-        ArrayList<Integer> t1 = new ArrayList<>(Arrays.asList(0, 1, 2));
-        System.out.println(t1.size());
-        test.add(0);
-        test.add(1);
-        test.add(2);
-        test.print();
-        // System.out.println("my size " + test.size());
-        // test.remove(2);
-        // test.print();
-        // System.out.println(test.get(1));
-        test.clear();
-        test.print();
-        // System.out.println("my size " + test.size());
-        // System.out.println("t1 " + t1.size());
+        System.out.println();
     }
 }
